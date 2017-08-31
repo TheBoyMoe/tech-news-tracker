d81a254fc0bd3f5754d4b73540b651a02fc24e47
theboymo <billfero@gmail.com>
2017-08-31 09:43:14 +0100

    add dummy #read_menu_command to archive class to fix crash

--- lib/news_tracker/menu/archive.rb
+++ lib/news_tracker/menu/archive.rb
@@ -6,6 +6,14 @@ module NewsTracker
         puts 'Display the list of archive articles'
       end
 
+      def read_menu_command
+        # TODO
+        puts 'More to follow.....'
+      end
+
+      def process_command
+        # TODO
+      end
     end
   end
 end
