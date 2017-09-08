53019e0e64395696063ccf1e2c48f9d19bed8f40
theboymo <billfero@gmail.com>
2017-09-08 14:35:57 +0100

    fetch and display archived articles

--- lib/news_tracker/menu/archive.rb
+++ lib/news_tracker/menu/archive.rb
@@ -3,17 +3,56 @@ module NewsTracker
     class Archive
 
       def display
-        puts 'Display the list of archive articles'
+        <<~HEREDOC
+          #{line_break}
+          #{opening_line}
+          #{line_break}
+          #{build_article_list}
+          #{line_break}
+          #{select_article_number}
+          #{prompt_user_to_go_back}
+          #{line_break}
+        HEREDOC
       end
 
       def read_menu_command
-        # TODO
-        puts 'More to follow.....'
+        @command = $stdin.gets.strip.downcase
       end
 
       def process_command
         # TODO
+
       end
+
+      private
+        def build_article_list
+          str = ""
+          fetch_articles.each.with_index(1) do |article, i|
+            str += "  #{i}. #{article.title}\n"
+          end
+          str.gsub(/\n$/, '')
+        end
+
+        def fetch_articles
+          NewsTracker::Article.fetch_archive
+        end
+
+        def opening_line
+          "Displaying list of archived articles"
+        end
+
+        def select_article_number
+          "Enter a number between 1-#{fetch_articles.size} to view more detail"
+        end
+
+        def prompt_user_to_go_back
+          "Type 'back' to return to the main menu"
+        end
+
+        def line_break
+          "------------------------------------------------------------------"
+        end
+
     end
   end
 end
