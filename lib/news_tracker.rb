bbce449f7dc2e40e4a434b2c508075acaf2e434b
theboymo <billfero@gmail.com>
2017-09-08 20:47:07 +0100

    update news tracker require statements

--- lib/news_tracker.rb
+++ lib/news_tracker.rb
@@ -8,13 +8,10 @@ DB = {:conn => SQLite3::Database.new('db/articles.db')}
 
 module NewsTracker
   require 'news_tracker/rss_feed'
-<<<<<<< HEAD
-=======
   require 'news_tracker/menu/main'
   require 'news_tracker/menu/list'
   require 'news_tracker/menu/archive'
   require 'news_tracker/menu/common'
->>>>>>> refactor_cli
   require 'news_tracker/cli'
   require 'news_tracker/article'
 end
